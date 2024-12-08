c77a00042d33636a94a7aaefeca46fb0f6111500
Srinivas Batthula <srinivasbatthula05.official@gmail.com>
2024-12-08 13:14:29 +0530

    0.1.4

--- components/Login.js
+++ components/Login.js
@@ -65,6 +65,8 @@ export default function Login() {
             setLoading(true);
             setErr(null);
 
+            console.log(formData)
+
             try {
                 const endpoint = isRegistering ? 'https://todo-backend-1-4u6w.onrender.com/api/auth/signUp' : 'https://todo-backend-1-4u6w.onrender.com/api/auth/signIn'; // Choose API based on mode
                 const response = await fetch(endpoint, {
