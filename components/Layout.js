804c0fdf208d8f010bb601402472cc091699e89c
Srinivas Batthula <srinivasbatthula05.official@gmail.com>
2024-12-09 20:59:01 +0530

    0.6.5

--- components/Layout.js
+++ components/Layout.js
@@ -16,6 +16,12 @@ const Layout = ({ children }) => {
     const { FetchData } = useContext(DataContext)
 
     useEffect(async() => {
+        setTimeout(()=>{
+            <div style={{width:'100%', height:'100vh', display:'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column', textAlign: 'center', backgroundColor: 'rgb(0, 153, 255)', color: 'white'}}>
+                <div style={{fontSize:'1.8rem', marginBottom: '0.5rem'}}>WELCOME TO YOUR</div>
+                <div style={{fontSize:'2rem'}}><strong>Task Manager</strong></div>
+            </div>
+        }, 400)
         setLoad(true)
         await FetchData()
         setLoad(false)
@@ -37,9 +43,13 @@ const Layout = ({ children }) => {
                     <div >
                         <Login />
                     </div>
-                ) : (load===true) ? (<div style={{width:'100vw', height:'100vh', display:'flex', justifyContent:'center', alignContent:'center', alignItems:'center', justifyItems:'center'}}>
-                    <span className={styles.loader}></span>
-                </div>) : ( <div>
+                ) : (load===true) ? (
+                    <div style={{overflow:'hidden'}}>
+                        <div style={{width:'100vw', height:'100vh', display:'flex', flexDirection:'column', justifyContent:'center', alignContent:'center', alignItems:'center', justifyItems:'center'}}>
+                            <span className={styles.loader}></span>
+                            <div style={{color:'#f45555', textAlign:'center', fontSize:'1.3rem', fontWeight:'600', marginTop:'0.6rem'}}>Fetching Data...</div>
+                        </div>
+                    </div>  ) : ( <div>
                         <main style={{
                             backgroundColor: theme,
                             color: theme === 'white' ? 'rgb(39, 39, 39)' : 'white',
