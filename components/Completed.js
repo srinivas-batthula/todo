1597ad6f1daf29890d92627b2d6e8ee7761faf01
Srinivas Batthula <srinivasbatthula05.official@gmail.com>
2024-12-10 19:23:23 +0530

    0.8.3

--- components/Completed.js
+++ components/Completed.js
@@ -50,7 +50,7 @@ export default function Complete(){
 
 
     return(
-        <div style={{width:'100vw',height:'100vh', top:'0.3rem', display:'flex', margin:'auto', overflowX:'hidden', overflowY:'scroll', scrollbarWidth:'thin', scrollBehavior:'smooth'}}>
+        <div style={{width:'100%',height:'100vh', top:'0.3rem', marginLeft:'1rem', marginRight:'1rem', overflowX:'hidden', overflowY:'scroll', scrollbarWidth:'thin', scrollBehavior:'smooth'}}>
             <div style={{marginBottom:'5rem', display:'flex', flexDirection:'column', justifyContent:'center', alignContent:'center', alignItems:'center', textAlign:'center'}}>
                 <div style={{fontSize:'1.3rem', fontWeight:'600', textAlign:'center', marginBottom:'0.8rem', marginTop:'0.65rem'}}>Your Completed Tasks : </div>
                 {
